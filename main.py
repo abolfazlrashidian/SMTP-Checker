2436539bf38757c4a8e803b8b8b7f12da4d4c056
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-07-18 16:21:36 +0330

    SMTP Checker added

--- main.py
+++ main.py
@@ -24,3 +24,34 @@ def extract_credential(filepath):
     except Exception:
         print('Cant open file')
 
+
+
+def check_smtp(credential):
+    USERNAME = credential['username']
+    PASSWORD = credential['password']
+    HOST = credential['host']
+    PORT = credential['port']
+    MSG = "This is smtp checker"
+
+    try:
+        with SMTP(HOST, PORT, timeout=10) as smtp_server:
+            smtp_server.starttls()
+            smtp_server.login(user=USERNAME,
+                            password=PASSWORD)
+            smtp_server.sendmail(from_addr=credential['username'], 
+                                to_addrs=email, 
+                                msg=MSG)
+        print('-'*100, f'\n[✅] VALID CREDENTIAL FOUND : {HOST}|{PORT}|{USERNAME}|{PASSWORD}')
+        with open(output, 'a') as file:
+            file.write(f'{HOST}|{PORT}|{USERNAME}|{PASSWORD}\n')
+            file.close()
+    except Exception:
+        print('-'*100, '\n[⚠️] INVALID CREDENTIAL !')
+
+
+def main():
+    extract_credential(r'SMTP-CHECKER\success.txt')
+    for i in credential.values():
+        check_smtp(i)
+
+main()
